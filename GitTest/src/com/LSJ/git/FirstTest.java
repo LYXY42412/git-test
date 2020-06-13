860f4ac3a327af18afcf86a58bb6250a436f6675
LSJ <1285505724@qq.com>
2020-06-13 18:16:32 +0800

    修改GitTest项目

--- GitTest/src/com/LSJ/git/FirstTest.java
+++ GitTest/src/com/LSJ/git/FirstTest.java
@@ -4,4 +4,8 @@ public class FirstTest {
     public  static void main(String[] agrs ){
         System.out.println("hello!");
     }
+    public  void test(){
+        int i=1;
+        i+=5;
+    }
 }
